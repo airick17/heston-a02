e6e887bf7540265ec683517f169c61c918a16248
Eric Heston <eric.heston98@gmail.com>
2021-11-09 19:35:55 -0500

    working solution on first compile XD

---
+++ exercise22/src/main/java/baseline/NumberCompare.java
@@ -0,0 +1,49 @@
+package baseline;
+
+import java.util.Scanner;
+import java.util.stream.DoubleStream;
+
+public class NumberCompare {
+    //Holds 2 ints
+    private int firstNum;
+    private int secondNum;
+    private int thirdNum;
+
+    //getter setters
+    public int getFirstNum() {return firstNum;}
+    public void setFirstNum(int firstNum) {this.firstNum = firstNum;}
+    public int getSecondNum() {return secondNum;}
+    public void setSecondNum(int secondNum) {this.secondNum = secondNum;}
+    public int getThirdNum() {return thirdNum;}
+    public void setThirdNum(int thirdNum) {this.thirdNum = thirdNum;}
+
+
+    //main start up method
+    public void doEverything() {
+        userNumbersEntry();
+        findLargestNum();
+        printLargestNum();
+    }
+
+    //gets users nums
+    private void userNumbersEntry() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the first number: ");
+        setFirstNum(sc.nextInt());
+        System.out.print("Enter the second number: ");
+        setSecondNum(sc.nextInt());
+        System.out.print("Enter the third number: ");
+        setThirdNum(sc.nextInt());
+    }
+
+    //finds largest num out of the 3
+    //found this easy way to call max on 3 nums
+    private int findLargestNum() {
+        return (int) DoubleStream.of(getFirstNum(), getSecondNum(), getThirdNum()).max().getAsDouble();
+    }
+
+    //prints output to user
+    private void printLargestNum() {
+    System.out.printf("The largest number is %d.%n", findLargestNum());
+    }
+}
---
+++ exercise22/src/main/java/baseline/Solution22.java
@@ -0,0 +1,15 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 2 Solution
+ *  Copyright 2021 Eric Heston
+ */
+
+package baseline;
+
+public class Solution22 {
+    public static void main(String[] args) {
+
+        NumberCompare nc = new NumberCompare();
+        nc.doEverything();
+
+    }
+}
